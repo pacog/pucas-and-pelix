d1beaa18343562bcc19f9cd6669ba916b4ac976d
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-01-25 10:49:50 +0100

    Correct title

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
     return (
         <div className="App">
-            <h1>Vite + React</h1>
+            <h1>Pucas & Pelix</h1>
         </div>
     );
 }
