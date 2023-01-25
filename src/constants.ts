21101918344eba6fd8b8157de14db2cf27aaa1d1
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-01-25 14:14:18 +0100

    Simplifying example, fixing types

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { SupportedModels, movenet } from "@tensorflow-models/pose-detection";
+
+export const MODEL = SupportedModels.MoveNet;
+export const MODEL_TYPE = movenet.modelType.SINGLEPOSE_LIGHTNING;
+export const SCORE_THRESHOLD = 0.3;
+export const BACKEND: BACKEND_TYPES = "tfjs-webgl";
+export const DEFAULT_LINE_WIDTH = 2;
+export const DEFAULT_RADIUS = 4;
+
+export type BACKEND_TYPES = "tfjs-webgl" | "tfjs-wasm" | "tfjs-webgpu";
--- src/main.ts
+++ src/main.ts
@@ -1,121 +1,26 @@
-/**
- * @license
- * Copyright 2021 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 import "@tensorflow/tfjs-backend-webgl";
 import "@tensorflow/tfjs-backend-webgpu";
-import * as mpPose from "@mediapipe/pose";
-
 import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
+import * as posedetection from "@tensorflow-models/pose-detection";
+import { Camera } from "./camera";
+import { setBackendAndEnvFlags } from "./util";
+import { MODEL, MODEL_TYPE } from "./constants";
 
 tfjsWasm.setWasmPaths(
     `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`
 );
 
-import * as posedetection from "@tensorflow-models/pose-detection";
-
-import { Camera } from "./camera";
-import { setupDatGui } from "./option_panel";
-import { STATE } from "./params";
-import { setBackendAndEnvFlags } from "./util";
-
-let detector, camera, stats;
-let startInferenceTime,
-    numInferences = 0;
-let inferenceTimeSum = 0,
-    lastPanelUpdate = 0;
-let rafId;
+let detector: posedetection.PoseDetector | null;
+let camera: Camera;
+let startInferenceTime = 0;
+let inferenceTimeSum = 0;
+let numInferences = 0;
+let lastPanelUpdate = 0;
 
 async function createDetector() {
-    switch (STATE.model) {
-        case posedetection.SupportedModels.PoseNet:
-            return posedetection.createDetector(STATE.model, {
-                quantBytes: 4,
-                architecture: "MobileNetV1",
-                outputStride: 16,
-                inputResolution: { width: 500, height: 500 },
-                multiplier: 0.75,
-            });
-        case posedetection.SupportedModels.BlazePose:
-            const runtime = STATE.backend.split("-")[0];
-            if (runtime === "mediapipe") {
-                return posedetection.createDetector(STATE.model, {
-                    runtime,
-                    modelType: STATE.modelConfig.type,
-                    solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}`,
-                });
-            } else if (runtime === "tfjs") {
-                return posedetection.createDetector(STATE.model, {
-                    runtime,
-                    modelType: STATE.modelConfig.type,
-                });
-            }
-        case posedetection.SupportedModels.MoveNet:
-            let modelType;
-            if (STATE.modelConfig.type == "lightning") {
-                modelType =
-                    posedetection.movenet.modelType.SINGLEPOSE_LIGHTNING;
-            } else if (STATE.modelConfig.type == "thunder") {
-                modelType = posedetection.movenet.modelType.SINGLEPOSE_THUNDER;
-            } else if (STATE.modelConfig.type == "multipose") {
-                modelType = posedetection.movenet.modelType.MULTIPOSE_LIGHTNING;
-            }
-            const modelConfig = { modelType };
-
-            if (STATE.modelConfig.customModel !== "") {
-                modelConfig.modelUrl = STATE.modelConfig.customModel;
-            }
-            if (STATE.modelConfig.type === "multipose") {
-                modelConfig.enableTracking = STATE.modelConfig.enableTracking;
-            }
-            return posedetection.createDetector(STATE.model, modelConfig);
-    }
-}
-
-async function checkGuiUpdate() {
-    if (STATE.isTargetFPSChanged || STATE.isSizeOptionChanged) {
-        camera = await Camera.setupCamera(STATE.camera);
-        STATE.isTargetFPSChanged = false;
-        STATE.isSizeOptionChanged = false;
-    }
-
-    if (STATE.isModelChanged || STATE.isFlagChanged || STATE.isBackendChanged) {
-        STATE.isModelChanged = true;
-
-        window.cancelAnimationFrame(rafId);
-
-        if (detector != null) {
-            detector.dispose();
-        }
-
-        if (STATE.isFlagChanged || STATE.isBackendChanged) {
-            await setBackendAndEnvFlags(STATE.flags, STATE.backend);
-        }
-
-        try {
-            detector = await createDetector(STATE.model);
-        } catch (error) {
-            detector = null;
-            alert(error);
-        }
-
-        STATE.isFlagChanged = false;
-        STATE.isBackendChanged = false;
-        STATE.isModelChanged = false;
-    }
+    return posedetection.createDetector(MODEL, {
+        modelType: MODEL_TYPE,
+    });
 }
 
 function beginEstimatePosesStats() {
@@ -136,10 +41,11 @@ function endEstimatePosesStats() {
 }
 
 async function renderResult() {
+    // TODO: extract
     if (camera.video.readyState < 2) {
         await new Promise((resolve) => {
             camera.video.onloadeddata = () => {
-                resolve(video);
+                resolve(true);
             };
         });
     }
@@ -148,7 +54,7 @@ async function renderResult() {
 
     // Detector can be null if initialization failed (for example when loading
     // from a URL that does not exist).
-    if (detector != null) {
+    if (!!detector) {
         // FPS only counts the time it takes to finish estimatePoses.
         beginEstimatePosesStats();
 
@@ -156,7 +62,7 @@ async function renderResult() {
         // contain a model that doesn't provide the expected output.
         try {
             poses = await detector.estimatePoses(camera.video, {
-                maxPoses: STATE.modelConfig.maxPoses,
+                maxPoses: 1,
                 flipHorizontal: false,
             });
         } catch (error) {
@@ -170,37 +76,23 @@ async function renderResult() {
 
     camera.drawCtx();
 
-    // The null check makes sure the UI is not in the middle of changing to a
-    // different model. If during model change, the result is from an old model,
-    // which shouldn't be rendered.
-    if (poses && poses.length > 0 && !STATE.isModelChanged) {
+    if (poses && poses.length > 0) {
         camera.drawResults(poses);
     }
 }
 
 async function renderPrediction() {
-    await checkGuiUpdate();
-
-    if (!STATE.isModelChanged) {
-        await renderResult();
-    }
-
-    rafId = requestAnimationFrame(renderPrediction);
+    await renderResult();
+    requestAnimationFrame(renderPrediction);
 }
 
 async function app() {
-    // Gui content will change depending on which model is in the query string.
-    const urlParams = new URLSearchParams(window.location.search);
-    if (!urlParams.has("model")) {
-        alert("Cannot find model in the query string.");
-        return;
-    }
+    camera = await Camera.setupCamera({
+        targetFPS: 60,
+        size: { width: 640, height: 480 },
+    });
 
-    await setupDatGui(urlParams);
-
-    camera = await Camera.setupCamera(STATE.camera);
-
-    await setBackendAndEnvFlags(STATE.flags, STATE.backend);
+    await setBackendAndEnvFlags();
 
     detector = await createDetector();
 
