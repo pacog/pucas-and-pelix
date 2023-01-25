2013206667019f08ecbd1a3fc9f46318e4e9eff7
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-01-25 14:16:28 +0100

    More simplification

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,6 @@ export const SCORE_THRESHOLD = 0.3;
 export const BACKEND: BACKEND_TYPES = "tfjs-webgl";
 export const DEFAULT_LINE_WIDTH = 2;
 export const DEFAULT_RADIUS = 4;
+export const MAX_POSES = 4;
 
 export type BACKEND_TYPES = "tfjs-webgl" | "tfjs-wasm" | "tfjs-webgpu";
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
 import * as posedetection from "@tensorflow-models/pose-detection";
 import { Camera } from "./camera";
 import { setBackendAndEnvFlags } from "./util";
-import { MODEL, MODEL_TYPE } from "./constants";
+import { MODEL, MODEL_TYPE, MAX_POSES } from "./constants";
 
 tfjsWasm.setWasmPaths(
     `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`
@@ -12,32 +12,11 @@ tfjsWasm.setWasmPaths(
 
 let detector: posedetection.PoseDetector | null;
 let camera: Camera;
-let startInferenceTime = 0;
-let inferenceTimeSum = 0;
-let numInferences = 0;
-let lastPanelUpdate = 0;
 
 async function createDetector() {
     return posedetection.createDetector(MODEL, {
         modelType: MODEL_TYPE,
     });
-}
-
-function beginEstimatePosesStats() {
-    startInferenceTime = (performance || Date).now();
-}
-
-function endEstimatePosesStats() {
-    const endInferenceTime = (performance || Date).now();
-    inferenceTimeSum += endInferenceTime - startInferenceTime;
-    ++numInferences;
-
-    const panelUpdateMilliseconds = 1000;
-    if (endInferenceTime - lastPanelUpdate >= panelUpdateMilliseconds) {
-        inferenceTimeSum = 0;
-        numInferences = 0;
-        lastPanelUpdate = endInferenceTime;
-    }
 }
 
 async function renderResult() {
@@ -55,14 +34,11 @@ async function renderResult() {
     // Detector can be null if initialization failed (for example when loading
     // from a URL that does not exist).
     if (!!detector) {
-        // FPS only counts the time it takes to finish estimatePoses.
-        beginEstimatePosesStats();
-
         // Detectors can throw errors, for example when using custom URLs that
         // contain a model that doesn't provide the expected output.
         try {
             poses = await detector.estimatePoses(camera.video, {
-                maxPoses: 1,
+                maxPoses: MAX_POSES,
                 flipHorizontal: false,
             });
         } catch (error) {
@@ -70,8 +46,6 @@ async function renderResult() {
             detector = null;
             alert(error);
         }
-
-        endEstimatePosesStats();
     }
 
     camera.drawCtx();
