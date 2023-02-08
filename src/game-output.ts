3e1e0eca24df81e5ef4d0ed2fc508e6900f9cf32
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-02-08 15:15:48 +0100

    Painting points and lines

--- src/game-output.ts
+++ src/game-output.ts
@@ -1,5 +1,7 @@
+import { Keypoint } from "@tensorflow-models/pose-detection";
 import { Application, Graphics } from "pixi.js";
 import { GameWorld } from "./game-world";
+import { PucasPelixPlayer } from "./player";
 
 interface GameOutputOptions {
     getGameWorld: () => GameWorld;
@@ -34,25 +36,9 @@ export class GameOutput {
     }
 
     tick(gameWorld: GameWorld) {
-        console.log("tick", gameWorld.players.length);
         this.playerGraphics.clear();
         gameWorld.players.forEach((player) => {
-            if (!player.pose) {
-                return;
-            }
-            const leftEye = player.pose.keypoints.find(
-                (keypoint) => keypoint.name === "left_eye"
-            );
-            const rightEye = player.pose.keypoints.find(
-                (keypoint) => keypoint.name === "right_eye"
-            );
-            if (!leftEye || !rightEye) {
-                return;
-            }
-            this.playerGraphics.lineStyle(2, 0xffffff, 1);
-            this.playerGraphics.moveTo(...this.project(leftEye));
-            this.playerGraphics.lineTo(...this.project(rightEye));
-            this.playerGraphics.closePath();
+            this.paintPlayer(player);
         });
     }
 
@@ -62,4 +48,84 @@ export class GameOutput {
             coords.y + this.options.margins.vertical,
         ];
     }
+
+    paintPlayer(player: PucasPelixPlayer) {
+        const MIN_ACCURACY = 0.4;
+        if (!player.pose) {
+            return;
+        }
+        const usefulBaseKeypoints = player.pose.keypoints.filter(
+            (keypoint) => (keypoint.score || 0) > MIN_ACCURACY
+        );
+
+        // TODO: object that creates new keypoints from old ones
+
+        const pointsToPaint = [
+            "left_eye",
+            "right_eye",
+            "left_shoulder",
+            "right_shoulder",
+        ];
+        const linesToPaint = [
+            ["left_shoulder", "right_shoulder"],
+            ["left_shoulder", "right_shoulder"],
+            ["left_shoulder", "left_elbow", "left_wrist"],
+            ["right_shoulder", "right_elbow", "right_wrist"],
+        ];
+
+        const keypointsMap = new Map<string, Keypoint>();
+        for (const keypoint of usefulBaseKeypoints) {
+            if (keypoint.name) {
+                keypointsMap.set(keypoint.name, keypoint);
+            }
+        }
+
+        pointsToPaint.forEach((pointName) => {
+            const keypoint = keypointsMap.get(pointName);
+            if (!keypoint) {
+                return;
+            }
+            this.playerGraphics.beginFill(0xde3249, 1);
+            this.playerGraphics.drawCircle(...this.project(keypoint), 10);
+            this.playerGraphics.endFill();
+        });
+
+        linesToPaint.forEach((line) => {
+            const keypoints = line.map((pointName) =>
+                keypointsMap.get(pointName)
+            );
+            if (keypoints.some((keypoint) => !keypoint)) {
+                return;
+            }
+            this.playerGraphics.lineStyle(2, 0xffffff, 1);
+            keypoints.forEach((keypoint, index) => {
+                if (!keypoint) {
+                    return;
+                }
+                if (index === 0) {
+                    this.playerGraphics.moveTo(...this.project(keypoint));
+                } else {
+                    this.playerGraphics.lineTo(...this.project(keypoint));
+                }
+            });
+        });
+    }
 }
+
+// ("nose");
+// ("left_eye");
+// ("right_eye");
+// ("left_ear");
+// ("right_ear");
+// ("left_shoulder");
+// ("right_shoulder");
+// ("left_elbow");
+// ("right_elbow");
+// ("left_wrist");
+// ("right_wrist");
+// ("left_hip");
+// ("right_hip");
+// ("left_knee");
+// ("right_knee");
+// ("left_ankle");
+// ("right_ankle");
