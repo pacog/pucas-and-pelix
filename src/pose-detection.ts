f4cc94d68f91f08f5da38620f0d0a14dbccafa9e
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-02-22 11:18:02 +0100

    Removing unused TF backends

--- src/pose-detection.ts
+++ src/pose-detection.ts
@@ -4,12 +4,7 @@ import {
     createDetector,
 } from "@tensorflow-models/pose-detection";
 import "@tensorflow/tfjs-backend-webgl";
-import "@tensorflow/tfjs-backend-webgpu";
-import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
-
-tfjsWasm.setWasmPaths(
-    `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`
-);
+import * as tf from "@tensorflow/tfjs-core";
 
 export async function getDetector() {
     const detector = await createDetector(SupportedModels.MoveNet, {
@@ -19,43 +14,11 @@ export async function getDetector() {
     if (!detector) {
         throw new Error("Error creating pose detector");
     }
-    setBackendAndEnvFlags();
+    startTFBackend();
     return detector;
 }
 
-import * as tf from "@tensorflow/tfjs-core";
-import { BACKEND } from "./constants";
-
-export const TUNABLE_FLAG_VALUE_RANGE_MAP = {
-    WEBGL_VERSION: [1, 2],
-    WASM_HAS_SIMD_SUPPORT: [true, false],
-    WASM_HAS_MULTITHREAD_SUPPORT: [true, false],
-    WEBGL_CPU_FORWARD: [true, false],
-    WEBGL_PACK: [true, false],
-    WEBGL_FORCE_F16_TEXTURES: [true, false],
-    WEBGL_RENDER_FLOAT32_CAPABLE: [true, false],
-    WEBGL_FLUSH_THRESHOLD: [-1, 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
-    CHECK_COMPUTATION_FOR_ERRORS: [true, false],
-};
-/**
- * Reset the target backend.
- */
-async function resetBackend(backendName: string) {
-    const ENGINE = tf.engine();
-    if (!(backendName in ENGINE.registryFactory)) {
-        throw new Error(`${backendName} backend is not registered.`);
-    }
-
-    if (backendName in ENGINE.registry) {
-        const backendFactory = tf.findBackendFactory(backendName);
-        tf.removeBackend(backendName);
-        tf.registerBackend(backendName, backendFactory);
-    }
-
-    await tf.setBackend(backendName);
-}
-
-async function setBackendAndEnvFlags() {
+function startTFBackend() {
     const flagConfig = {
         WEBGL_CPU_FORWARD: true,
         WEBGL_FLUSH_THRESHOLD: -1,
@@ -67,9 +30,19 @@ async function setBackendAndEnvFlags() {
 
     tf.env().setFlags(flagConfig);
 
-    const [runtime, $backend] = (BACKEND + "").split("-");
+    const backendName = "webgl";
 
-    if (runtime === "tfjs") {
-        await resetBackend($backend);
+    const ENGINE = tf.engine();
+
+    if (!(backendName in ENGINE.registryFactory)) {
+        throw new Error(`${backendName} backend is not registered.`);
     }
+
+    if (backendName in ENGINE.registry) {
+        const backendFactory = tf.findBackendFactory(backendName);
+        tf.removeBackend(backendName);
+        tf.registerBackend(backendName, backendFactory);
+    }
+
+    return tf.setBackend(backendName);
 }
