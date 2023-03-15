a24a2a09dadfdf5452cff450041caf7fcb99817b
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-03-15 17:51:37 +0100

    Fix base 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/pucas-and-pelix",
+    base: "/pucas-and-pelix/",
 });
