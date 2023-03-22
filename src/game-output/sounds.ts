918c33e20d4a866fbd65259dfd3707fd53fc13c2
Paco Gómez Granados <pacogomezgranados@gmail.com>
2023-03-22 10:46:02 +0100

    Better sounds

--- src/game-output/sounds.ts
+++ src/game-output/sounds.ts
@@ -26,11 +26,19 @@ const appearSounds = new SoundSet([
     "./sounds/appear_1.mp3",
     "./sounds/appear_2.mp3",
     "./sounds/appear_3.mp3",
+    "./sounds/appear_4.mp3",
+    "./sounds/appear_5.mp3",
+    "./sounds/appear_6.mp3",
 ]);
 
 const destroyedSounds = new SoundSet([
-    "./sounds/destroy_1.mp3",
-    "./sounds/destroy_2.mp3",
+    "./sounds/destroy_3.mp3",
+    "./sounds/destroy_4.mp3",
+    "./sounds/destroy_5.mp3",
+    "./sounds/destroy_6.mp3",
+    "./sounds/destroy_7.mp3",
+    "./sounds/destroy_8.mp3",
+    "./sounds/destroy_9.mp3",
 ]);
 
 const playerHappySounds = new SoundSet([
@@ -39,9 +47,12 @@ const playerHappySounds = new SoundSet([
     "./sounds/yeah_3.mp3",
     "./sounds/yeah_4.mp3",
     "./sounds/yeah_5.mp3",
+    "./sounds/yeah_6.mp3",
+    "./sounds/yeah_7.mp3",
+    "./sounds/guay_pelix_1.mp3",
 ]);
 
-const dieNaturallySounds = new SoundSet(["./sounds/destroy_1.mp3"]);
+const dieNaturallySounds = new SoundSet(["./sounds/die_naturally_1.mp3"]);
 
 export function playObjectDestroyed() {
     destroyedSounds.playRandom();
